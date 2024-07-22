047aa6f11300a1d26c66bcb21e67b1fd8755be92
Dayanand <dayukori@gmail.com>
2024-07-22 23:38:13 +0530

    Updated mediaQueries upto min-720px

---
+++ app.js
@@ -0,0 +1,16 @@
+/*const screenWidth = 1536;
+const titleWidth = 778;
+const descriptionWidth = 750;
+
+function getWidth() {
+    let newScreen = screen.width;
+
+    let newTitle = (titleWidth / screenWidth) * newScreen;
+    let newDescription = (descriptionWidth / screenWidth) * newScreen;
+
+    document.querySelector('.heroTitle').style.width = `${newTitle}px`;
+    document.querySelector('.description').style.width = `${newDescription}px`;
+}
+
+window.onresize = getWidth;
+*/
